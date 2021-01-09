0e54c907ce2e71cc80a9ce7a647fe68ac8fb5b50
Jean <tedros.jean@gmail.com>
2021-01-09 05:19:23 +0100

    added 32 word-lenght

--- src/fixed_const.h
+++ src/fixed_const.h
@@ -3,16 +3,23 @@
 
 //8-bit word-lenght
 #define CHAR_WL 8
-//8-bit precision max encoding
+//8-bit max encoding
 #define CHAR_MAX_ENCODING 0x7F
-//8-bit precision min encoding
+//8-bit min encoding
 #define CHAR_MIN_ENCODING 0x80
 
 //16-bit word-lenght
 #define INT_WL 16
-//16-bit precision max encoding
+//16-bit max encoding
 #define INT_MAX_ENCODING 0x7FFF
-//16-bit precision min enconding
+//16-bit min enconding
 #define INT_MIN_ENCODING 0x8000
 
+//32-bit word-lenght
+#define LONG_WL 32
+//32-bit max encoding
+#define LONG_MAX_ENCODING 0xFFFFFFFF
+//32-bit min encoding 
+#define LONG_MIN_ENCODING 0x80000000
+ 
 #endif
